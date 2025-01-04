7aa18d2057ebedae0ce66edb2168bda622a8a949
Hledjolf <jim.luebke@gmail.com>
2025-01-03 19:56:14 -0500

    Added Graphical inventory
    I'm not all that thrilled with it, but it may be the best tk can do.  Maybe PyGame has better?

--- ClientCode.py
+++ ClientCode.py
@@ -141,6 +141,45 @@ def create_gui(username):
         window.quit()
 
     window.protocol("WM_DELETE_WINDOW", on_closing)
+
+    def create_inventory_grid(frame, inventory_manager, username):
+        inventory_frame = tk.Frame(frame, relief=tk.RAISED, bd=2)
+        inventory_frame.grid(row=4, column=5, columnspan=10, padx=5, pady=5)
+
+        inventory_label = tk.Label(inventory_frame, text="Inventory:")
+        inventory_label.grid(row=0, column=0, columnspan=5)
+
+        icons = {}
+    
+        def add_item_to_inventory(item):
+            inventory_manager.add_item(username, item)
+            update_inventory_grid()
+
+        def remove_item_from_inventory(item):
+            inventory_manager.remove_item(username, item)
+            update_inventory_grid()
+
+        def update_inventory_grid():
+            for widget in inventory_frame.winfo_children():
+                if isinstance(widget, tk.Button):
+                    widget.destroy()
+                    
+            inventory = inventory_manager.view_inventory(username)
+            for index, item in enumerate(inventory):
+                icon = icons.get(item, None)
+                if not icon:
+                    # Create a placeholder icon for the item
+                    image = Image.new('RGB', (50, 50), color = 'gray')
+                    icon = ImageTk.PhotoImage(image)
+                    icons[item] = icon
+
+                item_button = tk.Button(inventory_frame, image=icon, command=lambda item=item: remove_item_from_inventory(item))
+                item_button.grid(row=(index // 5) + 1, column=index % 5, padx=5, pady=5)
+
+        add_item_button = tk.Button(inventory_frame, text="Add Item", command=lambda: add_item_to_inventory("New Item"))
+        add_item_button.grid(row=1, column=6, padx=5, pady=5)
+
+        update_inventory_grid()
     
     # Initialize InventoryManager and create inventory grid
     inventory_manager = InventoryManager()
