29adfcc4f195e8e612760ffe77867b67f3772731
Hledjolf <jim.luebke@gmail.com>
2025-01-03 20:06:41 -0500

    Skills and Abilities Revamp

---
+++ Abilities.py
@@ -0,0 +1,13 @@
+# Abilities.py
+
+def sword_slash(targeted_monster):
+    if targeted_monster:
+        targeted_monster.hit_points -= 5
+        return f"Sword Slash hits {targeted_monster.username} for 5 HP!"
+    return "No targeted monster."
+
+def magic_missile(targeted_monster):
+    if targeted_monster:
+        targeted_monster.hit_points -= 5
+        return f"Magic Missile hits {targeted_monster.username} for 5 HP!"
+    return "No targeted monster."
