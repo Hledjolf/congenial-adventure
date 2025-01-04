6a8401a40b98d8493dcf4793671071bb5f278c04
Hledjolf <jim.luebke@gmail.com>
2025-01-03 20:48:35 -0500

    Update User.py
    Update Users with skill information

--- User.py
+++ User.py
@@ -4,6 +4,7 @@ import os
 class User:
     def __init__(self, username):
         self.username = username
+        self.race = "Human" # Default race
         self.level = 1
         self.hit_points = 100
         self.max_hp = 100
@@ -18,9 +19,18 @@ class User:
             "wisdom": 10,
             "intelligence": 10,
             "constitution": 10,
-            "charisma": 10
+            "charisma": 10,
+            "agility": 10
         }
         self.inventory = []
+        self.skills = {
+            "Blacksmithing": {"level": 1, "skill_XP": 0, "dependent_stat": "Strength"},
+            "Tactics": {"level": 1, "skill_XP": 0, "dependent_stat": "Intelligence"},
+            "Heavy Armor": {"level": 1, "skill_XP": 0, "dependent_stat": "Strength"},
+            "Medium Armor": {"level": 1, "skill_XP": 0, "dependent_stat": "Dexterity"},
+            "Light Armor": {"level": 1, "skill_XP": 0, "dependent_stat": "Agility"}
+        }
+
 
     def set_role(self, role):
         valid_roles = ["Explorer", "Champion", "Merchant", "Craftsman", "Raid Leader", "Guild Leader", "Noble"]
@@ -29,12 +39,20 @@ class User:
         else:
             raise ValueError(f"Invalid role. Valid roles are: {', '.join(valid_roles)}")
 
+    def set_race(self, race):
+        valid_races = ["Human", "Elf", "Dwarf", "Halfling"]
+        if race in valid_races:
+            self.race = race
+        else:
+            raise ValueError(f"Invalid race. Valid races are: {', '.join(valid_races)}")
+
 # Function to create and initialize client data file
 def create_client_data_file(user):
     filename = f"{user.username}.json"
     if not os.path.exists(filename):
         data = {
             "name": user.username,
+            "race": user.race,
             "level": user.level,
             "hit_points": user.hit_points,
             "max_hp": user.max_hp,
